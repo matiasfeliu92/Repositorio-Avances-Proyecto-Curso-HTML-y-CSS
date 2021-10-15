7b8279465bf332b47b1642618c8cff2fa8bf56c1
matiasfeliu92 <matumazparrote@gmail.com>
2021-10-15 12:52:17 -0300

    avances de animaciones con JQUERY

--- js/presentacionAnimada.js
+++ js/presentacionAnimada.js
@@ -5,3 +5,23 @@ $(document).ready(() => {
 $(document).ready(() => {
     $('.presentacionProyectos').fadeOut(13000)
 })
+
+$(document).ready(() => {
+    $('.presentacionSobreMi').fadeOut(13000)
+})
+
+$(document).ready(() => {
+    $('.presentacionContacto').fadeOut(13000)
+})
+
+$(document).ready(() => {
+    $('.presentacionSoftware').fadeOut(13000)
+})
+
+$(document).ready(() => {
+    $('.presentacionModelado').fadeOut(13000)
+})
+
+$(document).ready(() => {
+    $('.presentacionRender').fadeOut(13000)
+})
